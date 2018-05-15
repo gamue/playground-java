d080fd56657f797df6e4d49a301e55e6275551fb
Johannes Ganzenmüller <johannes.ganzenmueller@europace.de>
2018-05-15 13:36:31 +0200

    Added API documentation with Swagger

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ This project is thought as a playground for frameworks and tools. It also acts a
 - [Travis CI](https://travis-ci.org/) build configuration via `.travis.yml` 
   - Status of master-branch [![Build Status](https://travis-ci.org/gamue/playground.svg?branch=master)](https://travis-ci.org/gamue/playground)
 - [Spring Boot](https://projects.spring.io/spring-boot/) with JSON-Endpoint `FizzBuzzController`
-
+- [Swagger](https://swagger.io/) for API documentation. Once started, the docu is located at `http://localhost:8080/swagger-ui.html` 
   
--- src/main/java/de/gamue/Application.java
+++ src/main/java/de/gamue/Application.java
@@ -3,17 +3,20 @@ package de.gamue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @SpringBootApplication
 @RestController
+@ApiIgnore("Dummy API which shouldn't be part of the documentation.")
 public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
-  @RequestMapping("/")
+  @RequestMapping(value = "/", method = {RequestMethod.POST, RequestMethod.GET})
   public String home() {
     return "Welcome to the playground";
   }
