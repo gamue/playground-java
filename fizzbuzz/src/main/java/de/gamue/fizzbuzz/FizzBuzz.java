538fff81b877383b76aa62df4bf5d6cddaa9932c
Johannes Ganzenmüller <jganzenmller@ebay.com>
2019-02-23 08:45:59 +0100

    Updated to Java11 and most current versions of dependencies

--- fizzbuzz/src/main/java/de/gamue/fizzbuzz/FizzBuzz.java
+++ fizzbuzz/src/main/java/de/gamue/fizzbuzz/FizzBuzz.java
@@ -6,36 +6,34 @@ import java.util.stream.IntStream;
 
 public class FizzBuzz {
 
-  private static final int DIVIDER_FIZZ = 3;
-  private static final String TEXT_FIZZ = "fizz";
-  private static final int DIVIDER_BUZZ = 5;
-  private static final String TEXT_BUZZ = "buzz";
-  private static final int DIVIDER_FIZZBUZZ = DIVIDER_FIZZ * DIVIDER_BUZZ;
-  private static final String TEXT_FIZZBUZZ = TEXT_FIZZ + TEXT_BUZZ;
+    private static final int DIVIDER_FIZZ = 3;
+    private static final String TEXT_FIZZ = "fizz";
+    private static final int DIVIDER_BUZZ = 5;
+    private static final String TEXT_BUZZ = "buzz";
+    private static final int DIVIDER_FIZZBUZZ = DIVIDER_FIZZ * DIVIDER_BUZZ;
+    private static final String TEXT_FIZZBUZZ = TEXT_FIZZ + TEXT_BUZZ;
 
-  private FizzBuzz() {
-  }
-
-  public static List<String> getEntries(int maxNumber) {
-    if (maxNumber <= 0) {
-      throw new IllegalArgumentException("maxNumber is smaller than 1.");
+    private FizzBuzz() {
     }
 
-    List<String> entries = new ArrayList<>();
-    IntStream.range(1, maxNumber + 1).forEach(number -> entries.add(getValue(number)));
-    return entries;
-  }
+    public static List<String> getEntries(int maxNumber) {
+        if (maxNumber <= 0) {
+            throw new IllegalArgumentException("maxNumber is smaller than 1.");
+        }
 
-  private static String getValue(int number) {
-    if (number % DIVIDER_FIZZBUZZ == 0) {
-      return TEXT_FIZZBUZZ;
+        List<String> entries = new ArrayList<>();
+        IntStream.range(1, maxNumber + 1).forEach(number -> entries.add(getValue(number)));
+        return entries;
     }
-    else if (number % DIVIDER_BUZZ == 0) {
-      return TEXT_BUZZ;
+
+    private static String getValue(int number) {
+        if (number % DIVIDER_FIZZBUZZ == 0) {
+            return TEXT_FIZZBUZZ;
+        } else if (number % DIVIDER_BUZZ == 0) {
+            return TEXT_BUZZ;
+        } else if (number % DIVIDER_FIZZ == 0) {
+            return TEXT_FIZZ;
+        }
+        return String.valueOf(number);
     }
-    else if (number % DIVIDER_FIZZ == 0) {
-      return TEXT_FIZZ;
-    }
-    return String.valueOf(number);
-  }
 }
--- fizzbuzz/src/main/java/de/gamue/fizzbuzz/FizzBuzzController.java
+++ fizzbuzz/src/main/java/de/gamue/fizzbuzz/FizzBuzzController.java
@@ -3,24 +3,23 @@ package de.gamue.fizzbuzz;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import java.util.List;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/fizzbuzz")
 @Api(description = "The well-known fizz buzz word game", tags = {"FizzBuzz"})
 public class FizzBuzzController {
 
-  @RequestMapping(method = RequestMethod.POST,
-      produces = "application/json;charset=UTF-8")
-  @ResponseBody
-  @ApiOperation(value = "Execute the logic")
-  public List<String> execute(@RequestHeader @ApiParam(value = "The maximum number") int maxNumber) {
-    return FizzBuzz.getEntries(maxNumber);
-  }
+    @PostMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    @ApiOperation(value = "Execute the logic")
+    public List<String> execute(@RequestHeader @ApiParam(value = "The maximum number") int maxNumber) {
+        return FizzBuzz.getEntries(maxNumber);
+    }
 }
