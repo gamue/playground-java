38c285ec1245a2290d4c23934d5a5e16ed7412aa
Johannes Ganzenmueller <johannes.ganzenmueller@autoscout24.com>
2022-11-25 16:17:32 +0100

    Handle code exception with defined http-code

--- fizzbuzz/src/main/java/de/gamue/fizzbuzz/FizzBuzzController.java
+++ fizzbuzz/src/main/java/de/gamue/fizzbuzz/FizzBuzzController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,5 +25,10 @@ public class FizzBuzzController {
     @Operation(summary = "Execute the logic")
     public List<String> execute(@RequestHeader @Parameter(description = "The maximum number") int maxNumber) {
         return FizzBuzz.getEntries(maxNumber);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public void handleException(IllegalArgumentException e){
     }
 }
