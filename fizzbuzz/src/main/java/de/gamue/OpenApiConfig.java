0f0eeda4e76fd2feb17915259be45425292fb3fe
Johannes Ganzenmueller <johannes.ganzenmueller@autoscout24.com>
2022-11-28 16:17:50 +0100

    Set API documentation for common errors in a global way

--- fizzbuzz/src/main/java/de/gamue/OpenApiConfig.java
+++ fizzbuzz/src/main/java/de/gamue/OpenApiConfig.java
@@ -1,9 +1,20 @@
 package de.gamue;
 
+import de.gamue.fizzbuzz.ErrorResponse;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import org.springdoc.core.SpringDocAnnotationsUtils;
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 
 @Configuration
@@ -18,4 +29,27 @@ public class OpenApiConfig {
                         .version("1.0"));
     }
 
+    @Bean
+    public OpenApiCustomiser customerGlobalHeaderOpenApiCustomiser(OpenAPI api) {
+        return openApi -> {
+            openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+                ApiResponses apiResponses = operation.getResponses();
+                ApiResponse badRequestError = getErrorResponse("Bad Request", api);
+                apiResponses.addApiResponse(String.valueOf(HttpStatus.BAD_REQUEST.value()), badRequestError);
+            }));
+        };
+    }
+
+    private ApiResponse getErrorResponse(String description, OpenAPI api){
+        Schema errorResponseSchema = SpringDocAnnotationsUtils.extractSchema(
+                api.getComponents(),
+                ErrorResponse.class,
+                null,
+                null
+        );
+        MediaType mediaType = new MediaType().schema(errorResponseSchema);
+        return new ApiResponse()
+                .description(description)
+                .content(new Content().addMediaType(APPLICATION_JSON_VALUE, mediaType));
+    }
 }
