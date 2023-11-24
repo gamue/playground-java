70fc63fc35e6268c9f8a512fed8778701cfce9d9
Johannes Ganzenmüller <johannes.ganzenmueller@gmail.com>
2023-11-24 22:07:50 +0100

    Update json2pojo lib (#19)

--- json-schema-to-pojos/src/test/java/de/gamue/api/PersonTest.java
+++ json-schema-to-pojos/src/test/java/de/gamue/api/PersonTest.java
@@ -1,5 +1,10 @@
 package de.gamue.api;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import java.util.Set;
+import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +21,18 @@ public class PersonTest {
 
     Assertions.assertEquals(AGE, p.getAge());
     Assertions.assertEquals(NAME, p.getName());
+  }
+
+  @Test
+  public void testValidation() {
+    Validator validator =
+        Validation.byDefaultProvider()
+            .configure()
+            .messageInterpolator(new ParameterMessageInterpolator())
+            .buildValidatorFactory()
+            .getValidator();
+
+    Set<ConstraintViolation<Person>> violations = validator.validate(new Person());
+    Assertions.assertEquals(2, violations.size());
   }
 }
