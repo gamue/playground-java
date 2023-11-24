437cffe5d99f26ec6bf1d63e87f164214e198626
Johannes Ganzenmüller <johannes.ganzenmueller@gmail.com>
2023-11-24 22:04:57 +0100

    Update json2pojo lib

--- json-schema-to-pojos/build.gradle.kts
+++ json-schema-to-pojos/build.gradle.kts
@@ -1,22 +1,24 @@
 plugins {
-    id("org.jsonschema2dataclass") version "4.5.0"
+    id("org.jsonschema2dataclass") version "6.0.0"
 }
 
 dependencies {
-    implementation("javax.validation:validation-api:2.0.1.Final")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.0")
+    implementation("jakarta.validation:jakarta.validation-api:3.0.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
+    testImplementation("org.hibernate.validator:hibernate-validator:8.0.1.Final")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:${Versions.junit}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${Versions.junit}")
 }
 
 jsonSchema2Pojo {
-    source.setFrom(files("${projectDir}/src/main/resources/de/gamue"))
-
-    includeJsr303Annotations.set(true)
-    includeAdditionalProperties.set(false)
-    includeHashcodeAndEquals.set(true)
-    includeToString.set(true)
+    executions {
+        create("main") {
+            io.source.setFrom(files("${projectDir}/src/main/resources/de/gamue"))
+            methods.annotateJsr303Jakarta.set(true)
+            methods.additionalProperties.set(false)
+        }
+    }
 }
 
 spotless {
